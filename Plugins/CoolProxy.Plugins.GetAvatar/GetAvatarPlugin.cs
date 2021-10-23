ff0bdf97b83e1ee4d74d08d338329c0c795ea07d
StolenRuby <ruby@mobiusgrid.us>
2021-10-23 15:51:36 +0100

    GetAvatar plugin

---
+++ Plugins/CoolProxy.Plugins.GetAvatar/GetAvatarPlugin.cs
@@ -0,0 +1,135 @@
+﻿using OpenMetaverse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoolProxy.Plugins.GetAvatar
+{
+    public class GetAvatarPlugin : CoolProxyPlugin
+    {
+        private CoolProxyFrame Proxy;
+        public GetAvatarPlugin(SettingsManager settings, GUIManager gui, CoolProxyFrame frame)
+        {
+            Proxy = frame;
+            gui.AddToolButton("Hacks", "Retrieve Stored Avatar", retrieveStoredAvatar);
+        }
+
+        private void retrieveStoredAvatar(object sender, EventArgs e)
+        {
+            AvatarPickerSearchForm avatarPickerSearchForm = new AvatarPickerSearchForm();
+
+            avatarPickerSearchForm.StartPosition = FormStartPosition.CenterScreen;
+
+            if (avatarPickerSearchForm.ShowDialog() == DialogResult.OK)
+            {
+                Proxy.SayToUser("Retrieving the stored appearance of " + avatarPickerSearchForm.SelectedName);
+                GetAvatar(avatarPickerSearchForm.SelectedID, avatarPickerSearchForm.SelectedName);
+            }
+        }
+
+        public void GetAvatar(UUID avatar, string folder_name)
+        {
+            try
+            {
+                Dictionary<string, object> sendData = new Dictionary<string, object> {
+                        { "METHOD", "getavatar"},
+                        { "UserID", avatar.ToString() }
+                    };
+
+                string request_str = ServerUtils.BuildQueryString(sendData);
+
+                WebClient webClient = new WebClient();
+                webClient.UploadStringCompleted += (x, y) =>
+                {
+                    if (y.Error != null)
+                    {
+                        Proxy.SayToUser(y.Error.Message);
+                        return;
+                    }
+
+                    Dictionary<string, object> replyData = ServerUtils.ParseXmlResponse(y.Result);
+
+                    if (replyData.ContainsKey("result"))
+                    {
+                        var boom = (Dictionary<string, object>)replyData["result"];
+
+                        List<UUID> attachment_ids = new List<UUID>();
+
+                        foreach (var pair in boom)
+                        {
+                            if (pair.Key.StartsWith("_ap_"))
+                            {
+                                UUID[] keys = pair.Value.ToString().Split(',').Select(key => new UUID(key)).ToArray();
+                                attachment_ids.AddRange(keys);
+                            }
+                            else if (pair.Key.StartsWith("Wearable "))
+                            {
+                                string val = pair.Value.ToString().Split(':')[0];
+                                attachment_ids.Add(new UUID(val));
+                            }
+                        }
+
+                        UUID parent_id = Proxy.Inventory.SuitcaseID != UUID.Zero ?
+                            Proxy.Inventory.FindSuitcaseFolderForType(FolderType.Clothing) :
+                            Proxy.Inventory.FindFolderForType(FolderType.Clothing);
+
+                        UUID folder_id = UUID.Random();
+
+                        Proxy.OpenSim.XInventory.AddFolder(folder_name, folder_id, parent_id, Proxy.Agent.AgentID, FolderType.None, 0, (folder_added) =>
+                        {
+                            if (folder_added)
+                            {
+                                int count = 0;
+
+                                foreach (UUID id in attachment_ids)
+                                {
+                                    Proxy.OpenSim.XInventory.GetItem(id, avatar, (item) =>
+                                    {
+                                        if (item == null)
+                                        {
+                                            Proxy.SayToUser(id.ToString() + " was not found!");
+                                            count++;
+                                            return;
+                                        }
+
+                                        item.UUID = UUID.Random();
+                                        item.CreationDate = DateTime.UtcNow;
+                                        item.OwnerID = Proxy.Agent.AgentID;
+                                        item.ParentUUID = folder_id;
+
+                                        Proxy.OpenSim.XInventory.AddItem(item, (success) =>
+                                        {
+                                            if (++count == attachment_ids.Count)
+                                            {
+                                                Proxy.SayToUser("All done! Requesting folder contents...");
+                                                Proxy.Inventory.RequestFolderContents(parent_id, Proxy.Agent.AgentID, true, true, InventorySortOrder.ByDate, false);
+                                            }
+                                        });
+                                    });
+                                }
+                            }
+                        });
+                    }
+                };
+
+                string target_uri = Proxy.Network.CurrentSim.GridURI;
+                target_uri += "/avatar";
+
+                Uri uri = new Uri(target_uri);
+
+                uri.SetPort(8003); // boom now
+
+                webClient.UploadStringAsync(uri, "POST", request_str);
+            }
+            catch (Exception ex)
+            {
+                Proxy.SayToUser(ex.Message);
+                return;
+            }
+        }
+    }
+}

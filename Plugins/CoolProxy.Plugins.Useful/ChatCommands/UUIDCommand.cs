8ddbfb85d48404ae675a9d2b205f2e5c5659dacd
StolenRuby <ruby@mobiusgrid.us>
2022-06-05 08:15:54 +0100

    uuid chat command

---
+++ Plugins/CoolProxy.Plugins.Useful/ChatCommands/UUIDCommand.cs
@@ -0,0 +1,26 @@
+﻿using OpenMetaverse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolProxy.Plugins.Useful.ChatCommands
+{
+    public class UUIDCommand : Command
+    {
+        public UUIDCommand(CoolProxyFrame frame)
+        {
+            Proxy = frame;
+            CMD = ".uuid";
+            Name = "Generate UUID";
+            Description = "Generates a random UUID.";
+            Category = CommandCategory.Other;
+        }
+
+        public override string Execute(string[] args)
+        {
+            return UUID.Random().ToString();
+        }
+    }
+}

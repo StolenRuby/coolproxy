a37ebd118a9e2d90e719b506690ccf191f6665c4
StolenRuby <ruby@mobiusgrid.us>
2022-06-05 08:10:24 +0100

    md5 chat command

---
+++ Plugins/CoolProxy.Plugins.Useful/ChatCommands/MD5Command.cs
@@ -0,0 +1,33 @@
+﻿using OpenMetaverse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolProxy.Plugins.Useful.ChatCommands
+{
+    public class MD5Command : Command
+    {
+        public MD5Command(CoolProxyFrame frame)
+        {
+            Proxy = frame;
+            CMD = ".md5";
+            Name = "MD5 String";
+            Description = "MD5 a given string.\n\nUsage: .md5 <string>";
+            Category = CommandCategory.Other;
+        }
+
+        public override string Execute(string[] args)
+        {
+            if(args .Length < 2)
+            {
+                return "Usage: .md5 <string>";
+            }
+
+            args = args.Skip(1).ToArray();
+
+            return Utils.MD5String(string.Join(" ", args));
+        }
+    }
+}
